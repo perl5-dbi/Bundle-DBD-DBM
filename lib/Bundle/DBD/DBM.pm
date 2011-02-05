7d51bc71ef5dd331e0b8ae4613e102a746b29af9
Jens Rehsack <sno@NetBSD.org>
2011-02-05 13:05:01 +0000

    Import test/requirement bundle for DBD::DBM

---
+++ lib/Bundle/DBD/DBM.pm
@@ -0,0 +1,72 @@
+package Bundle::DBD::DBM;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Bundle::DBD::DBM - Bundle to expand test range of DBD::DBM
+
+=cut
+
+our $VERSION = '0.06';
+
+=head1 DESCRIPTION
+
+This module comes with a lot of requirements to allow DBD::DBM tests
+covering an enormous range of serializer and backend combinations.
+
+=head1 AUTHOR
+
+Jens Rehsack, C<< <rehsack at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-bundle-dbd-dbm at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Bundle-DBD-DBM>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Bundle::DBD::DBM
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Bundle-DBD-DBM>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Bundle-DBD-DBM>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Bundle-DBD-DBM>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Bundle-DBD-DBM/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Jens Rehsack.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of Bundle::DBD::DBM
